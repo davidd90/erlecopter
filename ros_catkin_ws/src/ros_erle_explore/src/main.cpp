6786e57f27846c251bcf9bde5ce472fec445bfd7
David Damm <d.damm90@gmx.de>
2017-06-26 21:02:04 +0200

    changed static distances to dynamic computation

--- ros_catkin_ws/src/ros_erle_explore/src/main.cpp
+++ ros_catkin_ws/src/ros_erle_explore/src/main.cpp
@@ -8,101 +8,171 @@
 #include <mavros_msgs/SetMode.h>
 #include <mavros_msgs/OverrideRCIn.h>
 #include <sensor_msgs/LaserScan.h>
+#include <nav_msgs/Odometry.h>
 #include <geometry_msgs/PoseStamped.h>
 
 ros::Publisher pub;
 ros::Subscriber scanSub;
+ros::Subscriber odomSub;
 ros::Publisher local_pos_pub;
 
-bool turnLeft=false;
-bool wait=false;
-bool follow=false;
+enum flightMode { find, follow, leftTurn_wait , leftTurn_turn };
+flightMode fMode=find;
+
+bool freeFlight= false;
 ros::Time lastTime;
 int waitCounter;
+bool odomReceived=false;
+bool distanceReached=false;
+double xStart;
+double yStart;
+int nearestScan;
+double freeFlightDistance;
+double lastReceivedDistanceToWall;
+
+
+void storeOdomData(const nav_msgs::Odometry odom){
+	if(!odomReceived){
+		odomReceived= true;
+		xStart= odom.pose.pose.position.x;
+		yStart= odom.pose.pose.position.y;
+	}else{
+		double x = odom.pose.pose.position.x;
+		double y = odom.pose.pose.position.y;	
+		if((pow(xStart-x,2)+pow(yStart-y,2))>freeFlightDistance){
+			odomSub.shutdown();
+			odomReceived=false;
+			distanceReached=true;
+		}
+	}
+}
+
+void flyStraight()
+{
+	mavros_msgs::OverrideRCIn msg;
+	msg.channels[0] = 1500;     //Roll
+	msg.channels[1] = 1450;    //Pitch
+	msg.channels[2] = 1500;   //Throttle
+	msg.channels[3] = 1500;        //Yaw
+	msg.channels[4] = 0;
+	msg.channels[5] = 0;
+	msg.channels[6] = 0;
+	msg.channels[7] = 0;
+	pub.publish(msg);
+}
+
+void turnRight()
+{
+	mavros_msgs::OverrideRCIn msg;
+	msg.channels[0] = 1500;     //Roll
+	msg.channels[1] = 1500;    //Pitch
+	msg.channels[2] = 1500;   //Throttle
+	msg.channels[3] = 1600;        //Yaw
+	msg.channels[4] = 0;
+	msg.channels[5] = 0;
+	msg.channels[6] = 0;
+	msg.channels[7] = 0;
+
+	pub.publish(msg); 
+}
+
+void turnLeft()
+{
+	mavros_msgs::OverrideRCIn msg;
+	msg.channels[0] = 1500;   //Roll
+	msg.channels[1] = 1500;   //Pitch
+	msg.channels[2] = 1500;   //Throttle
+	msg.channels[3] = 1390;   //Yaw
+	msg.channels[4] = 0;
+	msg.channels[5] = 0;
+	msg.channels[6] = 0;
+	msg.channels[7] = 0;
+
+	pub.publish(msg); 
+}
 
 void processLaserScan(const sensor_msgs::LaserScan scan){
 
 	int size = scan.ranges.size();
 
 	bool wallLeft=false;
-	double midRange = scan.ranges[320];
-
-	mavros_msgs::OverrideRCIn msg;
 
 
-	bool turnRight= false;
-	for( int i = 300; i < 420; i++){
-		if(!isnan(scan.ranges[i]) &&scan.ranges[i]<2.5){
-			turnRight = true;
-			break;
-		}
-	}
-	for( int i = 320; i < 384; i++){
-		if(!isnan(scan.ranges[i])&& scan.ranges[i]<5.0){
-			wallLeft = true;
-		}
-	}
-	if(!wallLeft && follow){
-		waitCounter=waitCounter+1;
-		if(waitCounter >40){
-			ROS_ERROR("Start turn");
-			msg.channels[0] = 1500;     //Roll
-			msg.channels[1] = 1500;    //Pitch
-			msg.channels[2] = 1500;   //Throttle
-			msg.channels[3] = 1300;        //Yaw
-			msg.channels[4] = 0;
-			msg.channels[5] = 0;
-			msg.channels[6] = 0;
-			msg.channels[7] = 0;
 
-			pub.publish(msg); 
-			return;			
-		}
+	
+	double droneSpaceHalfAngle= std::atan(0.5/2.0);
+	double droneSpaceHalfRange = int(droneSpaceHalfAngle/scan.angle_increment);
 
-	}
+	if(fMode==find){
+		// check if the space in front of the drone is free
+		double droneSpaceHalfAngle= std::atan(0.5/2.0);
+		double droneSpaceHalfRange = int(droneSpaceHalfAngle/scan.angle_increment);
 		
-	
-	if(!turnRight){
-
-		/*
-            geometry_msgs::PoseStamped pose;
-    	    pose.pose.position.x = 20;
-            pose.pose.position.y = 20;
-   	    pose.pose.position.z = 100;
-            local_pos_pub.publish(pose);
-	
-	    */
-
-    	    msg.channels[0] = 1500;     //Roll
-	    msg.channels[1] = 1450;    //Pitch
-	    msg.channels[2] = 1500;   //Throttle
-	    msg.channels[3] = 1500;        //Yaw
-	    msg.channels[4] = 0;
-	    msg.channels[5] = 0;
-	    msg.channels[6] = 0;
-	    msg.channels[7] = 0;
-
-	    pub.publish(msg);
-	
-
-	}else{
-	    ROS_ERROR("Turn right%f",scan.ranges[0]);
-
-	    waitCounter=0;	
-	    follow=true;
- 	    msg.channels[0] = 1500;     //Roll
-	    msg.channels[1] = 1500;    //Pitch
-	    msg.channels[2] = 1500;   //Throttle
-	    msg.channels[3] = 1600;        //Yaw
-	    msg.channels[4] = 0;
-	    msg.channels[5] = 0;
-	    msg.channels[6] = 0;
-	    msg.channels[7] = 0;
-
-	    pub.publish(msg); 
-    	   	
+		for( int i = size/2-droneSpaceHalfRange; i < size/2+droneSpaceHalfRange; i++){
+			//if space in front of drone is not free then turn
+			if(!isnan(scan.ranges[i]) &&scan.ranges[i]<2){	
+			    	turnRight();
+		  	
+				fMode=follow;
+				break;
+			}
+		}
+		flyStraight();	
 	}
+	//check if the drone sees a wall to its left
+	for( int i = size/2; i < size; i++){
+		if(!isnan(scan.ranges[i])){	
+			if(wallLeft==false){
+				wallLeft = true;
+				lastReceivedDistanceToWall=scan.ranges[i];
+				nearestScan=i;
+				continue;
+			}
+			if(scan.ranges[i]<lastReceivedDistanceToWall){
+				lastReceivedDistanceToWall=scan.ranges[i];
+				nearestScan=i;
+			}
+		}
+	}
+
+	if (fMode==follow){
+		
+		if(!wallLeft){
+			fMode=leftTurn_wait;
+			double angle = (nearestScan-size/2)*scan.angle_increment;
+			freeFlightDistance= std::cos(angle)*lastReceivedDistanceToWall+0.5;
+ 			flyStraight();
+			ros::NodeHandle n;
+			odomSub =n.subscribe<nav_msgs::Odometry>("/mavros/local_position/odom",10,storeOdomData);	
+			return;
+			
+		}else{			
+			double realDistanceToWall=std::sin((nearestScan-size/2)*scan.angle_increment)*lastReceivedDistanceToWall;
+
+			ROS_ERROR("Distance: %f",realDistanceToWall);
+			if(realDistanceToWall<1){
+				    turnRight();
+			}else{
+				    flyStraight();			
+			}
+
+
+		}
+	}else if (fMode==leftTurn_wait ){
+		
+		if(distanceReached){
+			if (wallLeft){
+				distanceReached=false;
+				fMode=follow;
+			}else{
+				turnLeft();
+			}
+		}
+		return;				
+	}		
 }
+
+
 
 int main(int argc, char **argv)
 {
@@ -135,7 +205,6 @@ int main(int argc, char **argv)
     local_pos_pub = n.advertise<geometry_msgs::PoseStamped>
             ("mavros/setpoint_position/local", 10);
     scanSub =n.subscribe<sensor_msgs::LaserScan>("/scan",10,processLaserScan);
-  
     ros::spin();
 }
 
